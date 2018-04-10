a536cd23c375ec1c2b2b52a19284312c9c29794e
Michael McCulloch <mmcculloch@DESKTOP-6RI5IF7.hitronhub.home>
2018-04-10 10:52:25 -0600

    Create connections

---
+++ src/distrib.py
@@ -0,0 +1,13 @@
+import socket
+import sys
+import csv
+
+'''
+This class is responsible for sending and receiving data from other hosts
+'''
+class Distributor():
+    def __init__(self, master = 'localhost')
+
+    def send_message(message):
+
+    def receive_message():
--- src/master.py
+++ src/master.py
@@ -5,7 +5,7 @@ import sys
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_address = ('localhost', 10000)
+server_address = ('192.168.0.12', 10000)
 print('starting up on %s port %s' % server_address)
 sock.bind(server_address)
 
