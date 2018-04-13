0476520d4baabbcac4a4bb86f34e09eb46a562b5
Michael McCulloch <mmcculloch@DESKTOP-6RI5IF7.hitronhub.home>
2018-04-12 20:04:20 -0600

    Add skeleton for env, agent, verify basic genetic algorithm

---
+++ src/Agent.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class Agent():
+    def __init__(self, traits, env):
+        pass
+
+    def train(self, episode_count):
+        pass
+
+    def evaluate(self, num_tests=5):
+        return np.random.uniform()
--- src/main_train.py
+++ src/main_train.py
@@ -9,6 +9,9 @@ import numpy as np
 import sys
 import json
 
+from Agent import Agent
+from Environment import Environment
+
 COMM = MPI.COMM_WORLD
 size = COMM.Get_size()
 rank = COMM.Get_rank()
@@ -21,35 +24,23 @@ def chunks(l, n):
     for i in range(0, len(l), n):
         yield l[i:i + n]
 
+
 def mix_pop(population):
     '''
         gather() population into root node
         shuffle population
         scatter population
     '''
-    dict_pop = population
-    
-    all_pop = COMM.gather(dict_pop, root = 0)
-    if rank == 0:
-        #flatten gathered list of lists
-        all_pop = [b for val in all_pop for b in val]
-        shuffle(all_pop)
-        #chunk it for transmission
-        all_pop = list(chunks(all_pop, len(all_pop)//size))
-    new_pop = COMM.scatter(all_pop, root = 0)
-    return new_pop
 
-def evaluate(population):
-    
-    fitness = list()
-    for individual in population:
-        '''
-        Create an agent using the parameters of this individual
-        train it for 'short train' episodes, and evaluate it's performance on 5
-        make sure agent and memory is discarded after evaluation
-        '''
-        fitness.append(np.random.uniform(), individual)
-    return fitness
+    all_pop = COMM.gather(population, root=0)
+    if rank == 0:
+        # flatten gathered list of lists
+        all_pop = [b for val in all_pop for b in val]
+        shuffle(all_pop, np.random.uniform)
+        # chunk it for transmission
+        all_pop = list(chunks(all_pop, len(all_pop)//size))
+    new_pop = COMM.scatter(all_pop, root=0)
+    return new_pop
 
 def make_population():
     '''
@@ -57,37 +48,56 @@ def make_population():
     Assume config.json is mirrored across nodes
     '''
 
-    pop_size    = config['Population_Size']
-    gene_param   = config['Genetic_Parameters']
-    low_NHU, high_NHU       = gene_param['Num_Hidden_Units_Range']
-    n_Layers                = gene_param['Max_Conv_Layers']
-    kernel_max, stride_Max  = gene_param['Layer_KS_Limits']
+    pop_size = config['Population_Size']
+    gene_param = config['Genetic_Parameters']
+    low_NHU, high_NHU = gene_param['Num_Hidden_Units_Range']
+    n_Layers = gene_param['Max_Conv_Layers']
+    kernel_max, stride_Max = gene_param['Layer_KS_Limits']
 
-    rand_h_size         = np.random.randint(low_NHU, high_NHU, pop_size)
-    rand_layer_count    = np.random.randint(2, n_Layers+1, pop_size)
-    rand_kern           = np.random.randint(2, kernel_max+1, pop_size * n_Layers)
-    rand_stride         = np.random.randint(2, stride_Max+1, pop_size * n_Layers)
-    rand_kern           = np.reshape(rand_kern, [pop_size, -1])
-    rand_stride         = np.reshape(rand_stride, [pop_size, -1])
-    return list(zip(rand_h_size, rand_layer_count, rand_kern, rand_stride))
+    initial_fitness = np.repeat(None, pop_size)
+    rand_h_size = np.random.randint(low_NHU, high_NHU, pop_size)
+    rand_layer_count = np.random.randint(2, n_Layers+1, pop_size)
+    rand_kern = np.random.randint(2, kernel_max+1, pop_size * n_Layers)
+    rand_stride = np.random.randint(2, stride_Max+1, pop_size * n_Layers)
+    rand_kern = np.reshape(rand_kern, [pop_size, -1])
+    rand_stride = np.reshape(rand_stride, [pop_size, -1])
+    individuals = [{"Hidden_Unit_Size": h, "Layer_Count": l, "Kernel_Size": k, "Stride_Length": s}
+                   for (h, l, k, s) in zip(rand_h_size, rand_layer_count, rand_kern, rand_stride)]
+    return list(zip(initial_fitness, individuals))
 
+def breed(gene_pool, target_size, p_mutate = 0.8):
+    shuffle(gene_pool)
+    return [(None, h[1]) for h in gene_pool][0:target_size]
 
-if __name__== '__main__':
+if __name__ == '__main__':
+
     config = json.load(open("config.json"))
+    env = Environment(config)
     population = make_population()
 
-    for i in range(config['Generations']):
-        '''
-            1) evaluate fitness of population
-            2) while new_pop.size < "population_Size":
-                individuals chosen with roulette wheel
-                either:
-                a) Mutate single individual and append to new pop
-                b) crossover 2 individuals and append to new pop
-            3) population = new_pop
-        '''
-        if i != 0 and i % config['GeneratMix_Intervalions'] == 0:
-            mix_pop(population)
+    for gen in range(config['Generations']):
+        #Occasionally mix population among workers
+        if gen != 0 and gen % config['Mix_Interval'] == 0:
+            population = mix_pop(population)
 
+        #generate a list of candidates
+        candidates = list()
+        for individual in population:
+            fitness = individual[0]
+            traits = individual[1]
 
+            # don't evaluate an individual more than once
+            if fitness != None:
+                candidates.append(individual)
+                continue
 
+            agent = Agent(traits, env)
+            agent.train(episode_count=config['Short_Train'])
+            fitness = agent.evaluate()
+            candidates.append((fitness, traits))
+        
+        population = breed(candidates, len(population), p_mutate=config['Mutation_Prob'])
+    
+    '''
+    Here, select the best individual, and begin distributed training
+    '''
